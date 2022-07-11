e7152cf5e5fc31eeaaa3fb04c9f9fdd3941f27a8
Erinn East <erinn.east@gmail.com>
2022-07-11 17:38:10 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstName = "Erinn";
+let lastName = "East";
+
+let fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+let population = 672351;
+
+console.log(population);
+
+console.log(population *=3);
+
+let javaScriptIsCool = true;
+
+let colors = ["Red","Orange","Yellow", "Green", "Blue", "Indigo", "Violet"];
+
+
+for (let i = 1; i < 6; i++) {
+    console.log(i)
+  }
+
+  for (let i = 0; i < 5; i++) {
+    console.log(i)
+  }
